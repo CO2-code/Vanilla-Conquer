17fc9ad2d4960b8078b00366f98ffa7a4606c3a2
Maikel <test@test.test>
2021-02-05 17:25:41 +0100

    Create replay file dialog and test code hooked to load game menu

--- redalert/init.cpp
+++ redalert/init.cpp
@@ -60,6 +60,7 @@
 #include "language.h"
 #include "msgbox.h"
 #include "loaddlg.h"
+#include "replaydlg.h"
 
 #ifdef NETWORKING
 #include "wsproto.h"
@@ -809,7 +810,8 @@ bool Select_Game(bool fade)
             **	Load a saved game.
             */
             case SEL_LOAD_MISSION:
-                if (LoadOptionsClass(LoadOptionsClass::LOAD).Process()) {
+                if (LoadReplayClass().Process())
+                    {
                     Theme.Queue_Song(THEME_FIRST);
                     process = false;
                     gameloaded = true;
